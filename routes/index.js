68db1cf27f0bb73ff9b9e8a92cbf4f36d0290e29
alvdelci <delcimarioalvesdev@gmail.com>
2024-02-29 22:19:21 -0300

    Passando middleware de verificacao de email ja cadastrado

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const routes = Router();
 const { create, read, update, excluir } = require("../controllers");
+const { verifyEmail } = require("../middleware");
 
 
 routes.get("/", (req, res) => res.send("Servidor Online"))
-routes.post("/dev", create);
+routes.post("/dev", verifyEmail, create);
 routes.get("/dev", read);
 routes.put("/dev", update);
-routes.delete("/dev", excluir);
+routes.delete("/dev/:email", excluir);
 
 module.exports = routes;
